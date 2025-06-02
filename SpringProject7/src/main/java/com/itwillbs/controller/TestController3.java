e3c467a63cede0b283fc1330a457a7c73135c433
arumiiiiii <cocoahreum@gmail.com>
2025-06-02 11:43:22 +0900

    6666

---
+++ SpringProject7/src/main/java/com/itwillbs/controller/TestController3.java
@@ -0,0 +1,23 @@
+package com.itwillbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController3 {
+	@RequestMapping(value="/test6")
+	public void test6(){
+		
+	}//다시
+	
+	
+	@RequestMapping(value="/test7")
+	public void test7(){
+		
+	}//다시
+	
+	@RequestMapping(value="/test8")
+	public void test8(){
+		
+	}//다시  
+}
